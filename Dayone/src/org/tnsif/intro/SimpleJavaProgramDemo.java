d1ba6b299957220fef2a8c499b3c88af3835fbd5
Student <Student@IOE-AIDS-197>
2023-02-01 13:58:07 +0530

    first-commit

---
+++ Dayone/src/org/tnsif/intro/SimpleJavaProgramDemo.java
@@ -0,0 +1,12 @@
+package org.tnsif.intro;
+
+public class SimpleJavaProgramDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("WELCOME TO JAVA FULLSTACK PROGRAME");
+		
+
+	}
+
+}
