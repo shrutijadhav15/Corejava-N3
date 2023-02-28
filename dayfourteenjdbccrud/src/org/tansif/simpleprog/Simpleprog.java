8bdb8907541dcddf8d5fd96da234f9a113f00cb6
Supriya <Supriya@DESKTOP-TKP93L2>
2023-02-28 11:36:42 +0530

    first commit

---
+++ dayfourteenjdbccrud/src/org/tansif/simpleprog/Simpleprog.java
@@ -0,0 +1,11 @@
+package org.tansif.simpleprog;
+
+public class Simpleprog {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to java fullstack programe");
+
+	}
+
+}
